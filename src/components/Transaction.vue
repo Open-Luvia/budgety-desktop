e6bfcd3c1b9824c701e82491143ff237347811c8
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-04-15 12:00:09 +0200

    Style added to Transaction component

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,14 +1,14 @@
 <template>
    <div class="transaction">
-      <div class="date">
-         {{ date }}
+      <div class="info">
+         <div class="date">
+            {{ date }}
+         </div>
+         <div class="note">
+            {{ note }}
+         </div>
       </div>
-      <div class="note">
-         {{ note }}
-      </div>
-      <div class="amount">
-         {{ amount }} $
-      </div>
+      <div class="amount"> {{ amount }} $ </div>
    </div>
 </template>
 
@@ -27,21 +27,25 @@ export default {
 .transaction
     border-bottom: 1px solid black
     box-sizing: border-box
+    justify-content: space-between
     display: flex
     font-size: 20px
     font-weight: 500
-    justify-content: space-between
     margin-left: 15px
     margin-right: 15px
-    padding: 10px
-    .date
+    .info
        display: flex
-       align-items: center
-       justify-content: flex-start
-    .note
-       display: flex
-       align-items: center
-       justify-content: flex-start
+       width: calc( 100% - 120px )
+       .date
+          display: flex
+          align-items: center
+          justify-content: flex-start
+          flex-grow: 1
+       .note
+          flex-grow: 3
+          display: flex
+          align-items: center
+          justify-content: flex-start
     .amount
        background-color: map-get($colors, "positive-transaction")
        color: white
@@ -49,7 +53,7 @@ export default {
        align-items: center
        justify-content: center
        margin: 6px 0px 6px 0px
-       width: 5em
+       width: 120px
        height: 2em
        border-radius: 12px
 </style>
