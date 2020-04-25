be55673aa191809b44de4276df1d1d9b2dae1710
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-04-25 08:17:04 +0200

    Added the possibility to format without minus, useful for transaction edits

--- src/mixins/amountFormatter.mixin.js
+++ src/mixins/amountFormatter.mixin.js
@@ -13,6 +13,19 @@ export const amountFormatter = {
             result = result.concat('0')
          }
          return result
+      },
+      formatWithoutMinus(amount) {
+         var result = amount.toString()
+         if (result[0] == '-') {
+            result = result.slice(1)
+         }
+         if (
+            result.indexOf('.') != -1 &&
+            result[result.indexOf('.') + 2] == undefined
+         ) {
+            result = result.concat('0')
+         }
+         return result
       }
    }
 }
