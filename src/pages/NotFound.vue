28a573db0c2f3d0a355dd647472e4cb13b6e4d72
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-04-27 23:13:55 +0200

    Svg scaffolding added

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="container fullscreen">
-      <div class="icon">
-         <font-awesome-icon icon="exclamation" size="6x" />
-      </div>
+      <BaseIcon name="404" viewBox="0 0 1100 600" height="400" width="700">
+         <Icon404 />
+      </BaseIcon>
       <div class="title">
          <h1>Ooops!</h1>
          <h3>I tuoi soldi non sono qui</h3>
@@ -11,16 +11,23 @@
 </template>
 
 <script>
-export default {}
+import Icon404 from '@/assets/icons/Icon404'
+
+export default {
+   components: {
+      Icon404
+   }
+}
 </script>
 
 <style lang="sass" scoped>
 .container
     display: flex
+    flex-direction: column
     align-items: center
     justify-content: center
     .icon
         padding: 24px
     .title
-        text-align: left
+        text-align: center
 </style>
